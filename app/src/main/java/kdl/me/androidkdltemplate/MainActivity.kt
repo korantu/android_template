a81f9d47daffe2c1a2747a48acfc5f389a81a7d7
konstantin_levinski <konstantin_levinski@nec.com.sg>
2022-11-28 22:56:46 +0800

    feat: initial commit

---
+++ app/src/main/java/kdl/me/androidkdltemplate/MainActivity.kt
@@ -0,0 +1,15 @@
+package kdl.me.androidkdltemplate
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+  }
+
