9136b68717cf20f0228ac7b4debbd5948ee7b48a
konstantin_levinski <konstantin_levinski@nec.com.sg>
2022-11-28 23:43:01 +0800

    feat: committed simplest android app there is

--- app/src/main/java/kdl/me/androidkdltemplate/MainActivity.kt
+++ app/src/main/java/kdl/me/androidkdltemplate/MainActivity.kt
@@ -1,14 +1,29 @@
 package kdl.me.androidkdltemplate
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val tv = TextView(this)
+        tv.text = "Hi"
+
+        val btn = Button(this)
+        btn.text = "Do"
+
+        btn.setOnClickListener({
+            tv.text = "Done"
+        })
+
+        val ll = LinearLayout(this)
+        ll.addView(tv)
+        ll.addView(btn)
+        setContentView(ll)
 
     }
   }
